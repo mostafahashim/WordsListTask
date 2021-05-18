8354e433e532836ee9aa702ef29bda61ce2a0687
mostafa_hashim <mostafa.hashim90@gmail.com>
2021-05-18 20:45:19 +0200

    clean code

--- app/src/androidTest/java/words/list/task/view/activity/main/MainActivityTest.kt
+++ app/src/androidTest/java/words/list/task/view/activity/main/MainActivityTest.kt
@@ -1,17 +1,19 @@
 package words.list.task.view.activity.main
 
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.ComponentNameMatchers
-import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.web.sugar.Web
+import androidx.test.espresso.web.webdriver.DriverAtoms
+import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers
+import androidx.test.filters.LargeTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -20,6 +22,7 @@ import org.junit.runner.RunWith
 import words.list.task.R
 import words.list.task.TestUtil
 
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest{
     @get:Rule
@@ -46,6 +49,7 @@ class MainActivityTest{
                     ViewActions.click()
                 ))
     }
+
 
     @After
     fun shutDown() {
